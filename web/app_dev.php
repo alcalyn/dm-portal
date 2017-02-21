a78288d269d949ce168d1b21a8e8f38c5f58fcd5
Julien Maulny <doubjulien@hotmail.fr>
2017-02-21 19:48:16 +0100

    Install Docker (#3)
    * Install Docker
    * fix clone command
    * Remove app_dev checks for Docker
    * forbid access to app_dev with env variable

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,12 +8,7 @@ use Symfony\Component\Debug\Debug;
 // for more information
 //umask(0000);
 
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || php_sapi_name() === 'cli-server')
-) {
+if ('true' !== getenv('PORTAL_ENABLE_DEV')) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
